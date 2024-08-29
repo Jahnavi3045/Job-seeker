594b3487197bfda0b828733e22389247d47249ea
Jahnavi3045 <jahnavi3045@gmail.com>
2024-08-29 21:52:34 +0530

    chnages

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: 'https://job-seeker-frontend-production.up.railway.app', // Replace with your frontend's URL
-  optionsSuccessStatus: 200 ,
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true,// For some legacy browsers
-};
-app.use(cors(corsOptions));
-// app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// const corsOptions = {
+//   origin: 'https://job-seeker-frontend-production.up.railway.app', // Replace with your frontend's URL
+//   optionsSuccessStatus: 200 ,
+//   methods: ['GET', 'POST', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type'],
+//   credentials: true,// For some legacy browsers
+// };
+// app.use(cors(corsOptions));
+// // app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve static files from 'uploads' directory
 
